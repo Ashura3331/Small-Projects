3c4fec5dfca6df4a257fd61a0287134b2cc5772c
Ahsan Habib <freelancerashaa@gmail.com>
2024-12-16 19:04:01 +0600

    error correction

--- grade_calculator_project.js
+++ grade_calculator_project.js
@@ -2,17 +2,17 @@
 
 let number = Number(prompt("Enter your number"));
 
-if (number >= 90 || number <=100) {
+if (number >= 90 && number <=100) {
     console.log("You have got an A++!🎊🎉");
-}else if (number >= 80 || number <90) {
+}else if (number >= 80 && number <90) {
     console.log("You have got an A+🎊🎉!");
-}else if(number >= 70 || number < 80) {
+}else if(number >= 70 && number < 80) {
     console.log("You have got an A🎊🎉");
-}else if(number >= 60 || number < 70) {
+}else if(number >= 60 && number < 70) {
     console.log("You have got an A-🎊🎉");
-}else if(number >= 50 || number < 60) {
+}else if(number >= 50 && number < 60) {
     console.log("You have got an B🎊🎉");
-}else if(number >= 40 || number < 50) {
+}else if(number >= 40 && number < 50) {
     console.log("You have got an C🎊🎉");
 }else {
     console.log("You did not pass the exam!😰");
