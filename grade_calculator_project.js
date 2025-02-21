7f876d18c8c45f950fcbd1c38c7df82d948223d0
Ahsan Habib <freelancerashaa@gmail.com>
2025-02-21 21:23:15 +0600

    comments

--- grade_calculator_project.js
+++ grade_calculator_project.js
@@ -5,7 +5,7 @@
 // if (number >= 90 && number <=100) {
     // console.log("You have got an A++!🎊🎉");
 // }else if (number >= 80 && number <90) {
-    console.log("You have got an A+🎊🎉!");
+    // console.log("You have got an A+🎊🎉!");
 }else if(number >= 70 && number < 80) {
     console.log("You have got an A🎊🎉");
 }else if(number >= 60 && number < 70) {
