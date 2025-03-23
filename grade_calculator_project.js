f2f36bc05752c8d731f0b5050ebfe9b3a9b6f4ba
Ahsan Habib <freelancerashaa@gmail.com>
2025-03-23 23:03:56 +0600

    new comments

--- grade_calculator_project.js
+++ grade_calculator_project.js
@@ -1,11 +1,12 @@
 'use strict'
 
-// let number = Number(prompt("Enter your number"));
+let number = Number(prompt("Enter your number"));
 
-// if (number >= 90 && number <=100) {
-    // console.log("You have got an A++!🎊🎉");
+if (number >= 90 && number <=100) {
+    console.log("You have got an A++!🎊🎉");
 
 }else if (number >= 80 && number <90) {
+
     console.log("You have got an A+🎊🎉!");
 }else if(number >= 70 && number < 80) {
     console.log("You have got an A🎊🎉");
