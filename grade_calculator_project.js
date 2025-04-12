3f2510776e07750e21c8230cb849f02d3246377b
Ahsan Habib <freelancerashaa@gmail.com>
2025-04-12 22:42:03 +0600

    script lock

--- grade_calculator_project.js
+++ grade_calculator_project.js
@@ -7,7 +7,7 @@ let number = Number(prompt("Enter your number"));
 
 // }else if (number >= 80 && number <90) {
 
-    console.log("You have got an A+🎊🎉!");
+    // console.log("You have got an A+🎊🎉!");
 }else if(number >= 70 && number < 80) {
     console.log("You have got an A🎊🎉");
 }else if(number >= 60 && number < 70) {
